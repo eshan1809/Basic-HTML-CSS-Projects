35c4ae34088db72cf60cab2d84b555acd9f93f8e
Eshan Bhatt <43705525+eshan1809@users.noreply.github.com>
2020-12-24 11:37:45 +0530

    Add files via upload

---
+++ DOM/Dice Game/index.js
@@ -0,0 +1,15 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource = "images/dice" + randomNumber1 + ".png";
+document.querySelector(".img1").setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource = "images/dice" + randomNumber2 + ".png";
+document.querySelector(".img2").setAttribute("src", randomImageSource);
+
+if(randomNumber1 === randomNumber2){
+    document.querySelector("h1").innerHTML = "Draw!";
+} else if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+} else{
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
